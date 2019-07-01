ecc88ce645d7b6d18a903dd0e970dcadf30450f8
neddstarkk <nedheesh.hasija@gmail.com>
2019-07-01 10:10:29 +0530

    The file logic errors have been fixed.
    If you want to see results, make a folder 'audio' right next to the datagen folder and make an images folder there as well. After executing main.py audio files put in the audio folder will be converted to spectro images and saved in the images folder

--- datagen/generator.py
+++ datagen/generator.py
@@ -5,27 +5,27 @@ import pylab as pl
 i = 0
 
 
-def graph_spectrogram(wav_file):
-    global i 
-    i = i + 1
-    sound_info, frame_rate = get_wav_info(wav_file)
-    pl.figure(num=None, figsize=(19, 12))
-    pl.subplot(111)
-    pl.title('spectrogram of %r' % wav_file)
-    pl.specgram(sound_info, Fs=frame_rate)
-    pl.savefig(os.getcwd() + '/images/spectrogram' + str(i))
-def get_wav_info(wav_file):
-    wav = wave.open(wav_file, 'r')
-    frames = wav.readframes(-1)
-    sound_info = pylab.fromstring(frames, 'int16')
-    frame_rate = wav.getframerate()
-    wav.close()
-    return sound_info, frame_rate
+# def graph_spectrogram(wav_file):
+#     global i 
+#     i = i + 1
+#     sound_info, frame_rate = get_wav_info(wav_file)
+#     pl.figure(num=None, figsize=(19, 12))
+#     pl.subplot(111)
+#     pl.title('spectrogram of %r' % wav_file)
+#     pl.specgram(sound_info, Fs=frame_rate)
+#     pl.savefig(os.getcwd() + '/images/spectrogram' + str(i))
+# def get_wav_info(wav_file):
+#     wav = wave.open(wav_file, 'r')
+#     frames = wav.readframes(-1)
+#     sound_info = pl.fromstring(frames, 'int16')
+#     frame_rate = wav.getframerate()
+#     wav.close()
+#     return sound_info, frame_rate
 
-for (dir, subdir, files) in os.walk(os.getcwd()):
-    for file in files:
-        if file.endswith(".wav"):
-            graph_spectrogram(os.path.abspath("audio/" + file))
+# for (dir, subdir, files) in os.walk(os.getcwd()):
+#     for file in files:
+#         if file.endswith(".wav"):
+#             graph_spectrogram(os.path.abspath("audio/" + file))
 
 
 class DataGen:
@@ -33,7 +33,7 @@ class DataGen:
     def __init__(self):
         self.file_count = 0
         self.img_path_save = '/images/spectogram'
-        self.audio_path_save = 'audio/'
+        self.audio_path_save = '/audio/'
         self.figsize = (19,12)
         self.audio_format = '.wav'
 
@@ -49,7 +49,7 @@ class DataGen:
     def get_wave_info(self, wav_file):
         self.wav = wave.open(wav_file, 'r')
         self.frames = self.wav.readframes(-1)
-        self.sound_info = pl.fromstring(frames, 'int16')
+        self.sound_info = pl.fromstring(self.frames, 'int16')
         self.frame_rate = self.wav.getframerate()
         self.wav.close()
         return self.sound_info, self.frame_rate  
@@ -58,4 +58,5 @@ class DataGen:
         for (dir, subdir, files) in os.walk(os.getcwd()):
             for file in files:
                if file.endswith(self.audio_format):
-                    graph_spectrogram(os.path.abspath(self.audio_path_save + file))
+                   self.gen_spectrogram(os.getcwd() + self.audio_path_save + os.path.relpath(file))
+                    
